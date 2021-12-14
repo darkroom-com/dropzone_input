f055bbc7ae39bc59a7dafbe222da4fe4a5292262
Toby <toby@darkroom.tech>
2021-12-14 08:50:51 +0100

    stimulus-update: import from @hotwired/stimulus for 3.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 import { DirectUpload } from "@rails/activestorage";
 import Dropzone from "dropzone";
 import Cropper from 'cropperjs';
